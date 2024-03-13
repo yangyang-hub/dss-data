4367765db50b73fa5caf4e7abd625bfb013bd324
yangyang-hub <1348379987@qq.com>
2024-03-13 11:21:10 +0800

    20240313

--- robot/eastmoney.go
+++ robot/eastmoney.go
@@ -48,11 +48,6 @@ func getBkRelSymbol(bkCode string) *[]model.BkRelSymbol {
 }
 
 func getBkByPage(typeStr int, page, size int) (*[]model.Bk, *[]model.BkQuote) {
-	defer func() {
-		if r := recover(); r != nil {
-			log.Println("Recovered from panic:", r)
-		}
-	}()
 	url := "https://push2.eastmoney.com/api/qt/clist/get?fid=f62&po=1&pz=" + strconv.Itoa(size) + "&pn=" + strconv.Itoa(page) + "&np=1&fltt=2&invt=2&fs=m%3A90+t%3A" + strconv.Itoa(typeStr) + "&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f26,f22,f33,f11,f62,f128,f136,f115,f152,f124,f107,f104,f105,f140,f141,f207,f208,f209,f222"
 	respone := visitJson(url, "")
 	bks := []model.Bk{}
@@ -62,54 +57,13 @@ func getBkByPage(typeStr int, page, size int) (*[]model.Bk, *[]model.BkQuote) {
 		items := datas["diff"].([]interface{})
 		if len(items) > 0 {
 			for i, ite := range items {
-				bk := model.Bk{}
-				bk.Type = typeStr
-				bkQuote := model.BkQuote{}
-				bkQuote.TradeDate = time.Now().Format("20060102")
-				bkQuote.Rank = i + 1
-				item := ite.(map[string]interface{})
-				for key, value := range item {
-					switch key {
-					case "f12":
-						code := value.(string)
-						bk.Code = code
-						bkQuote.BkCode = code
-					case "f14":
-						bk.Name = value.(string)
-					case "f2":
-						bkQuote.Close = value.(float64)
-					case "f3":
-						bkQuote.PctChg = value.(float64)
-					case "f4":
-						bkQuote.Change = value.(float64)
-					case "f20":
-						bkQuote.Total = util.FloatDiv(value.(float64), 100000000)
-					case "f8":
-						bkQuote.Rate = value.(float64)
-					case "f104":
-						bkQuote.RiseCount = int(value.(float64))
-					case "f105":
-						bkQuote.FallCount = int(value.(float64))
-					case "f140":
-						bkQuote.Lead = value.(string)
-					case "f136":
-						bkQuote.LeadPctChg = value.(float64)
-					case "f17":
-						bkQuote.Open = value.(float64)
-					case "f15":
-						bkQuote.High = value.(float64)
-					case "f16":
-						bkQuote.Low = value.(float64)
-					case "f18":
-						bkQuote.PreClose = value.(float64)
-					case "f5":
-						bkQuote.Vol = util.FloatDiv(value.(float64), 10000)
-					case "f6":
-						bkQuote.Amount = util.FloatDiv(value.(float64), 100000000)
-					}
+				ibk, ibq := getBqItem(i, ite, typeStr)
+				if ibk != nil {
+					bks = append(bks, *ibk)
 				}
-				bks = append(bks, bk)
-				bkQuotes = append(bkQuotes, bkQuote)
+				if ibq != nil {
+					bkQuotes = append(bkQuotes, *ibq)
+				}
 			}
 		}
 	}
@@ -117,11 +71,6 @@ func getBkByPage(typeStr int, page, size int) (*[]model.Bk, *[]model.BkQuote) {
 }
 
 func getBkRelSymbolByPage(bkCode string, page, size int) *[]model.BkRelSymbol {
-	defer func() {
-		if r := recover(); r != nil {
-			log.Println("Recovered from panic:", r)
-		}
-	}()
 	url := "https://push2.eastmoney.com/api/qt/clist/get?fid=f62&po=1&pz=" + strconv.Itoa(size) + "&pn=" + strconv.Itoa(page) + "&np=1&fltt=2&invt=2&ut=b2884a393a59ad64002292a3e90d46a5&fs=b%3A" + bkCode + "&fields=f12%2Cf14%2Cf2%2Cf3%2Cf62%2Cf184%2Cf66%2Cf69%2Cf72%2Cf75%2Cf78%2Cf81%2Cf84%2Cf87%2Cf204%2Cf205%2Cf124%2Cf1%2Cf13"
 	respone := visitJson(url, "")
 	bkRelSymbols := []model.BkRelSymbol{}
@@ -130,19 +79,86 @@ func getBkRelSymbolByPage(bkCode string, page, size int) *[]model.BkRelSymbol {
 		items := datas["diff"].([]interface{})
 		if len(items) > 0 {
 			for _, ite := range items {
-				bkRelSymbol := model.BkRelSymbol{}
-				bkRelSymbol.BkCode = bkCode
-				item := ite.(map[string]interface{})
-				for key, value := range item {
-					switch key {
-					case "f12":
-						code := value.(string)
-						bkRelSymbol.Symbol = code
-					}
+				brl := getBrlItem(ite, bkCode)
+				if brl != nil {
+					bkRelSymbols = append(bkRelSymbols, *brl)
 				}
-				bkRelSymbols = append(bkRelSymbols, bkRelSymbol)
 			}
 		}
 	}
 	return &bkRelSymbols
+}
+
+func getBqItem(i int, ite interface{}, typeStr int) (*model.Bk, *model.BkQuote) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("Recovered from panic:", r)
+		}
+	}()
+	bk := model.Bk{}
+	bk.Type = typeStr
+	bkQuote := model.BkQuote{}
+	bkQuote.TradeDate = time.Now().Format("20060102")
+	bkQuote.Rank = i + 1
+	item := ite.(map[string]interface{})
+	for key, value := range item {
+		switch key {
+		case "f12":
+			code := value.(string)
+			bk.Code = code
+			bkQuote.BkCode = code
+		case "f14":
+			bk.Name = value.(string)
+		case "f2":
+			bkQuote.Close = value.(float64)
+		case "f3":
+			bkQuote.PctChg = value.(float64)
+		case "f4":
+			bkQuote.Change = value.(float64)
+		case "f20":
+			bkQuote.Total = util.FloatDiv(value.(float64), 100000000)
+		case "f8":
+			bkQuote.Rate = value.(float64)
+		case "f104":
+			bkQuote.RiseCount = int(value.(float64))
+		case "f105":
+			bkQuote.FallCount = int(value.(float64))
+		case "f140":
+			bkQuote.Lead = value.(string)
+		case "f136":
+			bkQuote.LeadPctChg = value.(float64)
+		case "f17":
+			bkQuote.Open = value.(float64)
+		case "f15":
+			bkQuote.High = value.(float64)
+		case "f16":
+			bkQuote.Low = value.(float64)
+		case "f18":
+			bkQuote.PreClose = value.(float64)
+		case "f5":
+			bkQuote.Vol = util.FloatDiv(value.(float64), 10000)
+		case "f6":
+			bkQuote.Amount = util.FloatDiv(value.(float64), 100000000)
+		}
+	}
+	return &bk, &bkQuote
+}
+
+func getBrlItem(ite interface{}, bkCode string) *model.BkRelSymbol {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("Recovered from panic:", r)
+		}
+	}()
+	bkRelSymbol := model.BkRelSymbol{}
+	bkRelSymbol.BkCode = bkCode
+	item := ite.(map[string]interface{})
+	for key, value := range item {
+		switch key {
+		case "f12":
+			code := value.(string)
+			bkRelSymbol.Symbol = code
+		}
+	}
+	return &bkRelSymbol
 }
